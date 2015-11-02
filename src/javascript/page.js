528bb50a4656a2b6860862c0b63db4e0b1652236
Stephan Gabler <stephan.gabler@gmail.com>
2015-11-02 11:07:25 -0800

    add a very simple D3 example

--- src/javascript/page.js
+++ src/javascript/page.js
@@ -7,10 +7,21 @@ var angular = require('angular'),
 
 var app = angular.module('bayes-webapp-base', [])
 
-app.controller('AppCtrl', function ($scope) {
+app.controller('AppCtrl', function ($scope, $http) {
 
   $scope.test = 'string value from scope'
   $scope.configValue = config.CONFIG_VARIABLE_1;
+
+  $http.get('/data/example_data.json').then(function (res) {
+    var d3Container = d3.select('#d3-container')
+    d3Container.selectAll('div.bar')
+      .data(res.data.values)
+      .enter()
+      .append('div')
+      .classed('bar', true)
+      .style('height', '10px')
+      .style('width', d => (d * 10) + 'px')
+  })
 
 })
 
